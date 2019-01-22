a984161e9ba0ce5eee7e98f56cc8fce77dfa9645
Malcolm Ramsay <malramsay64@gmail.com>
2019-01-23 10:51:27 +1100

    Implement intersection of lines and shapes
    This implements code for checking the intersection of both lines and
    ShapeInstances. When checking a shape, each of the lines of the shape
    are checked against each of the other.

--- src/shape.rs
+++ src/shape.rs
@@ -9,7 +9,7 @@ use nalgebra as na;
 use nalgebra::{IsometryMatrix2, Point2};
 use std::f64::consts::PI;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Shape {
     pub name: String,
     pub radial_points: Vec<f64>,
@@ -141,7 +141,7 @@ mod shape_tests {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct Line {
     pub start: Point2<f64>,
     pub end: Point2<f64>,
@@ -175,12 +175,136 @@ impl RelativeEq for Line {
     }
 }
 
-impl Line {
-    pub fn new(start: Point2<f64>, end: Point2<f64>) -> Self {
-        Self { start, end }
+impl std::fmt::Debug for Line {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Line {{ ({:.5}, {:.5}), ({:.5}, {:.5}) }}",
+            self.start[0], self.start[1], self.end[0], self.end[1]
+        )
     }
 }
 
+impl Line {
+    pub fn new(start: (f64, f64), end: (f64, f64)) -> Self {
+        Self {
+            start: Point2::new(start.0, start.1),
+            end: Point2::new(end.0, end.1),
+        }
+    }
+
+    pub fn intersects(&self, other: &Line) -> bool {
+        let a1_x = self.start[0];
+        let a1_y = self.start[1];
+        let a2_x = self.end[0];
+        let a2_y = self.end[1];
+        let b1_x = other.start[0];
+        let b1_y = other.start[1];
+        let b2_x = other.end[0];
+        let b2_y = other.end[1];
+
+        let ua_t = (b2_x - b1_x) * (a1_y - b1_y) - (b2_y - b1_y) * (a1_x - b1_x);
+        let ub_t = (a2_x - a1_x) * (a1_y - b1_y) - (a2_y - a1_y) * (a1_x - b1_x);
+        let u_b = (b2_y - b1_y) * (a2_x - a1_x) - (b2_x - b1_x) * (a2_y - a1_y);
+
+        if u_b != 0. {
+            let ua = ua_t / u_b;
+            let ub = ub_t / u_b;
+            println!("u_b: {}, ua: {}, ub: {}", u_b, ua, ub);
+            println!("ua <= 1.: {}", ua <= 1.);
+            println!("ua >= 0.: {}", ua >= 0.);
+            println!("ub >= 0.: {}", ub >= 0.);
+            println!("ub <= 1.: {}", ub <= 1.);
+            if ua >= 0. && ua <= 1. && ub >= 0. && ub <= 1. {
+                println!(
+                    "Intersect at: {:.4} {:.4}",
+                    a1_x + ua * (a2_x - a1_x),
+                    a1_y + ua * (a2_y - a1_y)
+                );
+                println!("Line1: {:?}, Line2: {:?}", self, other);
+                return true;
+            }
+        } else {
+            println!("u_b == 0");
+        }
+        false
+    }
+}
+
+#[cfg(test)]
+mod line_tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let line = Line::new((1., 0.), (0., 1.));
+        assert_eq!(line.start, Point2::new(1., 0.));
+        assert_eq!(line.end, Point2::new(0., 1.));
+    }
+
+    #[test]
+    fn intersects_radial() -> Result<(), String> {
+        // Testing lines to and from the same point don't intersect
+        // Using values of -1, 0, 1 for the x and y axes
+        let values: Vec<f64> = vec![-1., 0., 1.];
+        let points: Vec<(f64, f64)> = values
+            .iter()
+            .zip(values.iter())
+            .map(|(a, b)| (*a, *b))
+            .collect();
+
+        let mut result = Ok(());
+        for start1 in points.iter() {
+            for start2 in points.iter() {
+                let l1 = Line::new(*start1, (0., 0.));
+                let l2 = Line::new(*start2, (0., 0.));
+                if l1.intersects(&l2) {
+                    println!("{:?} {:?}", start1, start2);
+                    result = Err(String::from(""));
+                }
+            }
+        }
+        return result;
+    }
+
+    //
+    // +-------------------|-------------------+
+    // |                   |                   |
+    // |                   + 1                 |
+    // |                   |                   |
+    // |                   |                   |
+    // |                   |                   |
+    // |         p3        | p1      p2         |
+    // |---------+---------+---------+---------|
+    // |        -1         |         1         |
+    // |                   |                   |
+    // |                   |                   |
+    // |          p5       |                   |
+    // |         +      p4 + -1                |
+    // |                   |                   |
+    // |                   |                   |
+    // +-------------------|-------------------+
+    //
+    #[test]
+    fn intersects() {
+        let line1 = Line::new((-1., 0.), (0., -1.));
+        let line2 = Line::new((-1., -1.), (0., 0.));
+        assert!(line1.intersects(&line2));
+        assert!(line2.intersects(&line1));
+
+        let line3 = Line::new((-2., -1.), (1., 0.));
+        assert!(line2.intersects(&line3));
+        assert!(line3.intersects(&line2));
+        assert!(line1.intersects(&line3));
+        assert!(line3.intersects(&line1));
+    }
+
+}
+
+/// Puts an abstract shape object in a physical space
+///
+/// This matches a Shape to a transformation, placing it in 2D space.
+#[derive(PartialEq)]
 pub struct ShapeInstance<'a> {
     shape: &'a Shape,
     isometry: IsometryMatrix2<f64>,
@@ -209,6 +333,27 @@ impl<'a> ShapeInstance<'a> {
             .enumerate()
             .map(|(index, r)| self.radii_to_line(index, r))
             .collect()
+    }
+
+    pub fn intersects(&self, other: &ShapeInstance) -> bool {
+        // Same shape and same isometry => they intersect
+        if self == other {
+            return true;
+        }
+        for line_a in self.lines() {
+            for line_b in other.lines() {
+                if line_a.intersects(&line_b) {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+}
+
+impl<'a> std::fmt::Debug for ShapeInstance<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "ShapeInstance{{ {:?} }}", self.lines())
     }
 }
 
@@ -330,4 +475,49 @@ mod shape_instance_tests {
         }
     }
 
+    //
+    // +-------------------|-------------------+
+    // |                   |                   |
+    // |          +        + 1       +         |
+    // |           \       |       /           |
+    // |             \     |     /             |
+    // |               \   |   /               |
+    // |                 \ | /                 |
+    // |---------+---------+---------+---------|
+    // |        -1       / | \       1         |
+    // |               /   |   \               |
+    // |             /     |     \             |
+    // |           /       |       \           |
+    // |         +         + -1      +         |
+    // |                   |                   |
+    // |                   |                   |
+    // +-------------------|-------------------+
+    //
+    #[test]
+    fn intersects() {
+        let shape = Shape {
+            name: String::from("Square"),
+            radial_points: vec![1., 1., 1., 1.],
+            rotational_symmetries: 4,
+            mirrors: 4,
+        };
+        let shape_i1 = ShapeInstance {
+            shape: &shape,
+            isometry: IsometryMatrix2::new(na::Vector2::new(1., 0.), 0.),
+        };
+        assert!(shape_i1.intersects(&shape_i1));
+
+        let shape_i2 = ShapeInstance {
+            shape: &shape,
+            isometry: IsometryMatrix2::new(na::Vector2::new(-1.001, 0.), 0.),
+        };
+        assert!(!shape_i1.intersects(&shape_i2));
+
+        let shape_i3 = ShapeInstance {
+            shape: &shape,
+            isometry: IsometryMatrix2::new(na::Vector2::new(0., 0.), PI / 4.),
+        };
+        assert!(shape_i1.intersects(&shape_i3));
+        assert!(shape_i2.intersects(&shape_i3));
+    }
 }
