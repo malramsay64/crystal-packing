5e6b9cdac77b09e7b5b92d11fa2ae6110bb1e418
Malcolm Ramsay <malramsay64@gmail.com>
2019-01-28 14:52:22 +1100

    Refactor Line impls for clarity in Intersects
    This is primarily a refactoring of the Line struct to provide clarity on
    the implementation of the Intersects trait. This is a trait to allow a
    range of definitions of a shape to behave in much the same way. Taking a
    list of something that can check for intersections and comparing all
    against each other. This isn't the fastest method in all cases,
    (particularly lines) however it is simple and adaptable.

--- src/shape.rs
+++ src/shape.rs
@@ -9,141 +9,49 @@ use nalgebra as na;
 use nalgebra::{IsometryMatrix2, Point2};
 use std::f64::consts::PI;
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct RadialShape {
-    pub name: String,
-    pub radial_points: Vec<f64>,
-    pub rotational_symmetries: u64,
-    pub mirrors: u64,
-}
-
-impl<'a> IntoIterator for &'a RadialShape {
-    type Item = (f64, f64);
-    type IntoIter = RadialShapeIter<'a>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        Self::IntoIter::new(&self)
-    }
-}
-
-impl RadialShape {
-    pub fn area(&self) -> f64 {
-        // This is the sine of the angle between each point, this is used for every calculation
-        // so pre-calculate here.
-        let angle_term: f64 = f64::sin(2. * PI / self.radial_points.len() as f64);
-
-        self.into_iter()
-            .map(|(a, b)| 0.5 * angle_term * a * b)
-            .sum()
-    }
-
-    pub fn max_radius(&self) -> f64 {
-        self.radial_points
-            .iter()
-            .cloned()
-            // The f64 type doesn't have complete ordering because of Nan and Inf, so the
-            // standard min/max comparators don't work. Instead we use the f64::max which ignores
-            // the NAN and max values.
-            .fold(std::f64::MIN, f64::max)
-    }
-
-    pub fn radial_step(&self) -> f64 {
-        2. * PI / self.radial_points.len() as f64
-    }
-}
-
-/// Iterator for the RadialShape class
-///
-/// This iterates over all the line segments comprising a shape.
-pub struct RadialShapeIter<'a> {
-    shape: &'a RadialShape,
-    index: usize,
-    len: usize,
-}
-
-impl<'a> RadialShapeIter<'a> {
-    fn new(shape: &'a RadialShape) -> Self {
-        RadialShapeIter {
-            shape,
-            index: 0,
-            len: shape.radial_points.len(),
-        }
-    }
-}
-
-impl<'a> Iterator for RadialShapeIter<'a> {
-    type Item = (f64, f64);
-
-    fn next(&mut self) -> Option<(f64, f64)> {
-        if self.index >= self.len {
-            return None;
-        }
-        let result = Some((
-            self.shape.radial_points[self.index],
-            self.shape.radial_points[(self.index + 1) % self.len],
-        ));
-        self.index += 1;
-        result
-    }
-}
-
-#[cfg(test)]
-mod shape_tests {
-    use super::*;
-
-    fn create_square() -> RadialShape {
-        RadialShape {
-            name: String::from("Square"),
-            radial_points: vec![1., 1., 1., 1.],
-            rotational_symmetries: 4,
-            mirrors: 4,
-        }
-    }
-
-    #[test]
-    fn init() {
-        let square = create_square();
-        assert_eq!(square.name, "Square");
-        assert_eq!(square.radial_points, vec![1., 1., 1., 1.]);
-        assert_eq!(square.rotational_symmetries, 4);
-        assert_eq!(square.mirrors, 4);
-    }
-
-    #[test]
-    fn area() {
-        let square = create_square();
-        assert_abs_diff_eq!(square.area(), 2.);
-    }
-
-    #[test]
-    fn max_radius() {
-        let shape = RadialShape {
-            name: String::from("iter_test"),
-            radial_points: vec![1., 2., 3., 4.],
-            rotational_symmetries: 1,
-            mirrors: 0,
-        };
-        assert_abs_diff_eq!(shape.max_radius(), 4.);
-        assert_abs_diff_eq!(shape.max_radius(), 4.);
-    }
-
-    #[test]
-    fn iter_values() {
-        let shape = RadialShape {
-            name: String::from("iter_test"),
-            radial_points: vec![1., 2., 3., 4.],
-            rotational_symmetries: 1,
-            mirrors: 0,
-        };
-        let manual = vec![(1., 2.), (2., 3.), (3., 4.), (4., 1.)];
-        assert_eq!(shape.into_iter().collect::<Vec<(f64, f64)>>(), manual);
-    }
+trait Intersect {
+    fn intersects(&self, other: &Self) -> bool;
 }
 
 #[derive(Clone, Copy, PartialEq)]
 pub struct Line {
     pub start: Point2<f64>,
     pub end: Point2<f64>,
+}
+
+impl Intersect for Line {
+    /// Determine whether two line segments intersect
+    ///
+    /// This calculates whether two lines intersect at a point contained within each line segment
+    /// see [this](https://en.wikipedia.org/wiki/Intersection_%28Euclidean_geometry%29#Two_line_segments)
+    /// Wikipedia article for more information on the algorithm used for this calculation.
+    ///
+    fn intersects(&self, other: &Self) -> bool {
+        // Also see below links for other implementations of this algorithm
+        // - https://github.com/georust/geo/blob/96c7846d703a74f59ba68e68929415cbce4a68d9/geo/src/algorithm/intersects.rs#L142
+        // - https://github.com/brandonxiang/geojson-python-utils/blob/33b4c00c6cf27921fb296052d0c0341bd6ca1af2/geojson_utils.py
+        // - http://www.kevlindev.com/gui/math/intersection/Intersection.js
+        //
+        let u_b = other.dy() * self.dx() - other.dx() * self.dy();
+        // Where u_b == 0 the two lines are parallel. In this case we don't need any further checks
+        // since we are only concerned with lines that cross, parralel is fine.
+        if u_b == 0. {
+            return false;
+        }
+
+        let ua_t = other.dx() * (self.start.y - other.start.y)
+            - other.dy() * (self.start.x - other.start.x);
+        let ub_t =
+            self.dx() * (self.start.y - other.start.y) - self.dy() * (self.start.x - other.start.x);
+
+        let ua = ua_t / u_b;
+        let ub = ub_t / u_b;
+        // Should the points ua, ub both lie on the interval [0, 1] the lines intersect.
+        if 0. <= ua && ua <= 1. && 0. <= ub && ub <= 1. {
+            return true;
+        }
+        false
+    }
 }
 
 impl AbsDiffEq for Line {
@@ -211,39 +119,6 @@ impl Line {
     /// The diffence in the y values over the line.
     pub fn dy(&self) -> f64 {
         self.end.y - self.start.y
-    }
-
-    /// Determine whether two line segments intersect
-    ///
-    /// This calculates whether two lines intersect at a point contained within each line segment
-    /// see [this](https://en.wikipedia.org/wiki/Intersection_%28Euclidean_geometry%29#Two_line_segments)
-    /// Wikipedia article for more information on the algorithm used for this calculation.
-    ///
-    pub fn intersects(&self, other: &Line) -> bool {
-        // Also see below links for other implementations of this algorithm
-        // - https://github.com/georust/geo/blob/96c7846d703a74f59ba68e68929415cbce4a68d9/geo/src/algorithm/intersects.rs#L142
-        // - https://github.com/brandonxiang/geojson-python-utils/blob/33b4c00c6cf27921fb296052d0c0341bd6ca1af2/geojson_utils.py
-        // - http://www.kevlindev.com/gui/math/intersection/Intersection.js
-        //
-        let u_b = other.dy() * self.dx() - other.dx() * self.dy();
-        // Where u_b == 0 the two lines are parallel. In this case we don't need any further checks
-        // since we are only concerned with lines that cross, parralel is fine.
-        if u_b == 0. {
-            return false;
-        }
-
-        let ua_t = other.dx() * (self.start.y - other.start.y)
-            - other.dy() * (self.start.x - other.start.x);
-        let ub_t =
-            self.dx() * (self.start.y - other.start.y) - self.dy() * (self.start.x - other.start.x);
-
-        let ua = ua_t / u_b;
-        let ub = ub_t / u_b;
-        // Should the points ua, ub both lie on the interval [0, 1] the lines intersect.
-        if 0. <= ua && ua <= 1. && 0. <= ub && ub <= 1. {
-            return true;
-        }
-        false
     }
 }
 
