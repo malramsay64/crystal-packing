5ba0ecf8205cf7f1a10f43e9f5c19098cdfb5bc1
Malcolm Ramsay <malramsay64@gmail.com>
2019-02-04 12:20:55 +1100

    enh: Benchmarking the creation of a ShapeInstance
    The creation of a ShapeInstance is one of the components in the checking
    of an intersection for a shape so as such I would like to optimise it as
    much as possible.
    
    This is intended to provide information on what elements of the
    comparison are taking up all the time.

--- benches/intersection.rs
+++ benches/intersection.rs
@@ -4,11 +4,15 @@
 // Distributed under terms of the MIT license.
 //
 
+use std::f64::consts::PI;
+use std::ops::Mul;
+
 #[macro_use]
 extern crate criterion;
 
 use criterion::{Criterion, ParameterizedBenchmark};
 use nalgebra::Vector2;
+
 use packing::shape::{LineShape, Shape, ShapeInstance};
 use packing::symmetry::Transform;
 
@@ -41,9 +45,14 @@ fn setup_state(points: usize) -> packing::PackedState<LineShape> {
 fn setup_shapes<S>(shape: &S) -> (ShapeInstance<S::Component>, ShapeInstance<S::Component>)
 where
     S: Shape,
+    for<'a> S::Component: Mul<&'a Transform, Output = S::Component>,
+    for<'a, 'b> &'a S::Component: Mul<&'b Transform, Output = S::Component>,
+    for<'a> &'a S::Component: Mul<Transform, Output = S::Component>,
 {
-    let si1 = ShapeInstance::from(shape, Transform::new(Vector2::new(-2., 0.), 0.));
-    let si2 = ShapeInstance::from(shape, Transform::new(Vector2::new(2., 0.), 0.));
+    // These two shapes don't intersect so there is no shortcut out of the checking for
+    // intersection
+    let si1 = ShapeInstance::from(shape, &Transform::new(Vector2::new(-2., 0.), 0.));
+    let si2 = ShapeInstance::from(shape, &Transform::new(Vector2::new(2., 0.), 0.));
 
     (si1, si2)
 }
@@ -76,9 +85,25 @@ fn shape_check_intersection(c: &mut Criterion) {
     c.bench("test_bench_param", benchmark);
 }
 
+fn create_shape_instance(c: &mut Criterion) {
+    let parameters: Vec<usize> = (2..6).map(|x| 2_u64.pow(x) as usize).collect();
+
+    let benchmark = ParameterizedBenchmark::new(
+        "Creation of ShapeInstance",
+        |b, &param| {
+            let shape = create_polygon(param).unwrap();
+            let trans = Transform::new(Vector2::new(0.2, -5.3), PI / 3.);
+            b.iter(|| ShapeInstance::from(&shape, &trans))
+        },
+        parameters,
+    );
+    c.bench("create_shape_instance", benchmark);
+}
+
 criterion_group!(
     intersections,
     shape_check_intersection,
+    create_shape_instance,
     state_check_intersection,
 );
 criterion_main!(intersections);
