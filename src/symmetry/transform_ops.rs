961d4e99abdbfe2aa3e293016ec3a40b5530128f
Malcolm Ramsay <malramsay64@gmail.com>
2019-02-06 13:28:05 +1100

    maint: remove unneeded transform_ops module

--- src/symmetry/transform_ops.rs
+++
@@ -1,8 +0,0 @@
-//
-// symmetry_ops.rs
-// Copyright (C) 2019 Malcolm Ramsay <malramsay64@gmail.com>
-// Distributed under terms of the MIT license.
-//
-#![allow(clippy::suspicious_arithmetic_impl)]
-#![allow(clippy::op_ref)]
-use std::ops::{Add, Mul};
