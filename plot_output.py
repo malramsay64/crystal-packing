3dd043bf1cefab54fca6509bd2f010d44d832125
Malcolm Ramsay <malramsay64@gmail.com>
2019-06-04 12:14:39 +1000

    fix: Actually modify the plot_output script

--- plot_output.py
+++ plot_output.py
@@ -1,38 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from pathlib import Path
-import sys
-import matplotlib.pyplot as plt
 import re
+import sys
+from pathlib import Path
+
+import matplotlib.pyplot as plt
 
 remove_chars = "[" + re.escape("{}[]()") + "]"
+
 
 def main(filename):
     fig, ax = plt.subplots()
     with open(filename) as src:
         for line in src:
-            if line.startswith("Atom"):
-                line = re.sub(remove_chars, '', line[4:])
-                x, y, r, colour =[element.strip() for element in line.strip().split(',')]
-                circle = plt.Circle((float(x), float(y)), float(r), facecolor=colour,
-                    edgecolor=colour, linewidth=3, alpha=0.8)
+            if line.startswith("Atom2"):
+                line = re.sub(remove_chars, "", line[5:])
+                x, y, r, colour = [
+                    element.strip() for element in line.strip().split(",")
+                ]
+                circle = plt.Circle(
+                    (float(x), float(y)),
+                    float(r),
+                    facecolor=colour,
+                    edgecolor=colour,
+                    linewidth=3,
+                    alpha=0.8,
+                )
                 ax.add_patch(circle)
-            elif line.startswith("Line"):
-                line = re.sub(remove_chars, '', line[4:])
-                x0, y0, x1, y1, colour =[element.strip() for element in line.strip().split(',')]
+            elif line.startswith("Line2"):
+                line = re.sub(remove_chars, "", line[5:])
+                x0, y0, x1, y1, colour = [
+                    element.strip() for element in line.strip().split(",")
+                ]
                 ax.plot([float(x0), float(x1)], [float(y0), float(y1)], c=colour)
             else:
-                line = re.sub(remove_chars, '', line)
-                x0, y0, x1, y1, colour =[element.strip() for element in line.strip().split(',')]
+                line = re.sub(remove_chars, "", line)
+                x0, y0, x1, y1, colour = [
+                    element.strip() for element in line.strip().split(",")
+                ]
                 ax.plot([float(x0), float(x1)], [float(y0), float(y1)], c=colour)
-                
+
     ax.set_aspect(1)
     ax.plot(0, 0)
 
     fig.show()
 
-    plt.savefig(filename.with_suffix('.pdf'))
+    plt.savefig(filename.with_suffix(".pdf"))
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
@@ -47,4 +62,3 @@ if __name__ == "__main__":
         raise ValueError(f"File '{filename}' does not exist")
 
     main(filename)
-
