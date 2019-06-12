dff2cea825615aa67103a8a14f225956d958afd4
Malcolm Ramsay <malramsay64@gmail.com>
2019-06-12 14:37:59 +1000

    test: Include tests for the potential state
    This ensures the potential state is actually minimising the potentials
    of the configurations.

---
+++ tests/potential.rs
@@ -0,0 +1,67 @@
+//
+// potential.rs
+// Copyright (C) 2019 Malcolm Ramsay <malramsay64@gmail.com>
+// Distributed under terms of the MIT license.
+//
+
+use std::fs;
+
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use serde::Deserialize;
+use serde_json;
+
+use packing;
+#[allow(unused_imports)]
+use packing::traits::*;
+use packing::wallpaper::Wallpaper;
+use packing::wallpaper::WyckoffSite;
+use packing::{
+    monte_carlo_best_packing, Cell2, CrystalFamily, FromSymmetry, LJShape2, LineShape, MCVars,
+    OccupiedSite, PackedState, PotentialState, Transform2,
+};
+
+#[test]
+fn test_score_improves() -> Result<(), &'static str> {
+    let square = LJShape2::from_trimer(0.63, 120., 1.);
+
+    let wallpaper = Wallpaper {
+        name: String::from("p2"),
+        family: CrystalFamily::Monoclinic,
+    };
+
+    let isopointal = &[WyckoffSite {
+        letter: 'd',
+        symmetries: vec![
+            Transform2::from_operations("x,y").unwrap(),
+            Transform2::from_operations("-x,-y").unwrap(),
+        ],
+        num_rotations: 1,
+        mirror_primary: false,
+        mirror_secondary: false,
+    }];
+
+    let state =
+        PotentialState::<LJShape2, Cell2, OccupiedSite>::initialise(square, wallpaper, isopointal);
+
+    let init_score = state.score()?;
+
+    let vars = MCVars {
+        kt_start: 0.1,
+        kt_finish: 0.001,
+        max_step_size: 0.1,
+        num_start_configs: 1,
+        steps: 100,
+        seed: Some(0),
+    };
+
+    let final_state = monte_carlo_best_packing(&vars, state);
+
+    let final_score = final_state?.score()?;
+
+    println!("Init Score: {}, Final Score: {}", init_score, final_score);
+    assert!(init_score < final_score);
+
+    Ok(())
+}
