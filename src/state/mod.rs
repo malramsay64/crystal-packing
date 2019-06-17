87bee11fdaa03273d4a9f4284b48c15b19996761
Malcolm Ramsay <malramsay64@gmail.com>
2019-06-17 12:37:02 +1000

    refactor: Create state module for states
    This new module holds the potential and the packing states and will also
    hold the constructors for these.

---
+++ src/state/mod.rs
@@ -0,0 +1,11 @@
+//
+// mod.rs
+// Copyright (C) 2019 Malcolm Ramsay <malramsay64@gmail.com>
+// Distributed under terms of the MIT license.
+//
+
+pub mod packing;
+pub mod potential;
+
+pub use packing::*;
+pub use potential::*;
