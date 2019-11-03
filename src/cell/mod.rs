81aadddb6203fdf9db61d264cf617a09cd1ee6e8
Malcolm Ramsay <malramsay64@gmail.com>
2019-11-04 10:23:59 +1100

    refactor: Flatten structure of src
    This pulls the cell and site into a flatter namespace.

--- src/cell/mod.rs
+++
@@ -1,9 +0,0 @@
-//
-// mod.rs
-// Copyright (C) 2019 Malcolm Ramsay <malramsay64@gmail.com>
-// Distributed under terms of the MIT license.
-//
-
-pub mod cell2;
-
-pub use cell2::*;
--- src/site/mod.rs
+++
@@ -1,9 +0,0 @@
-//
-// mod.rs
-// Copyright (C) 2019 Malcolm Ramsay <malramsay64@gmail.com>
-// Distributed under terms of the MIT license.
-//
-
-mod site2;
-
-pub use site2::*;
