b5585e4a7bf7e2e08e349bd88ce6e6568c015129
Malcolm Ramsay <malramsay64@gmail.com>
2019-11-07 13:58:32 +1100

    fix: Unwrap final score for display
    This ensures the final score is unwrapped for display, removing the Some
    part.

--- src/main.rs
+++ src/main.rs
@@ -133,7 +133,12 @@ fn analyse_state(
         .max()
         .ok_or_else(|| anyhow!("Error in running optimisation."))?;
 
-    info!("Final score: {:?}", final_state.score());
+    info!(
+        "Final score: {}",
+        final_state
+            .score()
+            .ok_or_else(|| anyhow!("State has become corrupted"))
+    );
 
     let serialised = serde_json::to_string(&final_state)?;
 
